69f5f21a32ac14feab28bae0c183b640482a082a
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2024-12-25 16:48:53 +0200

    Add v3 factory

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,7 @@ const config = {
   DB_NAME: process.env.DB_NAME,
   FACTORY_ADDRESS: process.env.FACTORY_ADDRESS,
   FACTORY_V2_ADDRESS: process.env.FACTORY_V2_ADDRESS,
+  FACTORY_V3_ADDRESS: process.env.FACTORY_V3_ADDRESS,
   WVTRU_ADDRESS: process.env.WVTRU_ADDRESS,
   LOCAL_CHAIN: process.env.LOCAL_CHAIN,
   WS_CLIENT_API_PORT: process.env.WS_CLIENT_API_PORT,
